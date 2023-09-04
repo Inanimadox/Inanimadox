a6654fd994e9d5787692ed53a40e629bc1a2d9a7
Inanimadox <Inanimadox@outlook.com>
2023-09-04 17:50:45 -0300

    Basado re integro
    Descompresión de la carpeta que almacena Bazado!

---
+++ Basado! - copia/JavaScript/Main.js
@@ -0,0 +1,32 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/* al tocar boton*/
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// esperar al siguente click o mejor dicho cuenta de clics
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        
+      }
+    }
+  }
+}
